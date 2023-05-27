7ac70bbf9ef656be300267770cc5ffea02df41ab
28vikas <63vikas@gmail.com>
2023-05-27 21:17:08 +0530

    top resposive navbar

---
+++ top resposive navbar/app.js
@@ -0,0 +1,10 @@
+let humberger = document.querySelector('.humberger');
+humberger.onclick = function(){
+    navbar = document.querySelector('.navbar');
+    navbar.classList.toggle('active');
+}
+
+// function xyz(){
+//    navbar = document.querySelector('.navbar');
+//    navbar.classList.toggle('active')
+// } 
